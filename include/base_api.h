76cc13383daef4debeea2feeb558ba954badfb39
edghyhdz <edgar.hdz@hotmail.com>
2021-12-18 01:52:58 +0100

    Added set_id method

--- include/base_api.h
+++ include/base_api.h
@@ -12,16 +12,20 @@ class BaseAPIExchange {
    public:
     // Do get request and get Token/coin price data
     virtual std::string get_data() = 0;
-    // Process get response 
+    // Process get response
     virtual std::string process_response(std::string &response) = 0;
 
     // Normal behavior methods
     virtual int get_id() { return _id; }
     virtual std::string get_api_key() { return _api_key; }
 
+    virtual void set_id(int id) { this->_id = id; }
+
+   protected:
+    int _id;
+
    private:
     std::map<std::string, double> _processed_response;
-    int _id;
     std::string _api_key;
 };
 
