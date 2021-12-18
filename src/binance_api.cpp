f5b5fb3921e453ef218410fa419f64393aff030d
edghyhdz <edgar.hdz@hotmail.com>
2021-12-18 01:54:18 +0100

    Added mock method definitions

--- src/binance_api.cpp
+++ src/binance_api.cpp
@@ -1,2 +1,10 @@
 #include "binance_api.h"
 
+BinanceAPI::BinanceAPI(int id) { this->set_id(id); }
+
+std::string BinanceAPI::process_response(std::string &response) {
+    return response + " extra text";
+}
+
+std::string BinanceAPI::get_data() { return "{'result': 'x'}"; }
+
