757263d6eb8711e13d5ab83b8bb5a618d8e7bb4b
edghyhdz <edgar.hdz@hotmail.com>
2021-12-20 22:06:26 +0100

    Added manager class header

---
+++ include/manager.h
@@ -0,0 +1,19 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include "binance_api.h"
+#include "mongo_db.h"
+
+enum class API { BINANCE, KUCOIN };
+
+class ApiManager {
+   public:
+    void get_token_price_data(API api);
+    void save_data();
+
+   private:
+    MongoDB _db_handler;
+    BinanceAPI _binance_handler;
+};
+
+#endif
