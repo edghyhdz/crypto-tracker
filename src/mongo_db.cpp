941138adf412cf964c6c7286fbf8b27f35560711
edghyhdz <edgar.hdz@hotmail.com>
2021-12-20 21:53:52 +0100

    Doc string changes and error handling in add_record method

--- src/mongo_db.cpp
+++ src/mongo_db.cpp
@@ -5,7 +5,6 @@ MongoDB::MongoDB(std::string uri, std::string db_name)
     : _uri(mongocxx::uri(uri)),
       _client(mongocxx::client(_uri)),
       _db(_client[db_name]) {
-
     std::cout << "Constructor MongoDB" << std::endl;
 }
 
@@ -19,39 +18,43 @@ bool MongoDB::delete_record(std::string id) {
     return true;
 }
 
-///
-/// Adds currency price and current time stamp as a record into given collection
-/// name `coll_name`
-///
-/// @param[in] coll_name
-///   Collection name where data will be added to
-/// @param data
-///   Data struct containing map `dictionary` with currencies and prices to add
-/// @return True if transaction was performed successfully
-///
+/**
+ * Adds crypto currency price and timestamp as a new record in the given
+ * collection name `coll_name`
+ *
+ * @param[in] coll_name
+ *  Collection name where data will be added to
+ * @param data[in]
+ *  Data struct containing a `dictionary` map attribute that contains all
+ * currencies and prices to be added
+ * @return true if transaction was performed successfully, else false
+ */
 bool MongoDB::add_record(std::string coll_name, Data data) {
     // Get current time stamp from system
     auto system_clock = std::chrono::system_clock::now();
     long time_stamp = std::chrono::duration_cast<std::chrono::milliseconds>(
                           system_clock.time_since_epoch())
                           .count();
+    try {
+        mongocxx::collection collection = this->_db[coll_name];
+        bsoncxx::builder::stream::document builder{};
 
-    mongocxx::collection collection = this->_db[coll_name];
-    bsoncxx::builder::stream::document builder{};
+        auto init_doc = builder << "timeStamp" << time_stamp << "results"
+                                << bsoncxx::builder::stream::open_document;
 
-    auto init_doc = builder << "timeStamp" << time_stamp << "results"
-                            << bsoncxx::builder::stream::open_document;
+        // Add all records to builder
+        for (auto &&cd : data.dictionary) {
+            init_doc << cd.first << cd.second;
+        }
+        auto close_doc = init_doc << bsoncxx::builder::stream::close_document;
 
-    // Add all records to builder
-    for (auto &&cd : data.dictionary) {
-        init_doc << cd.first << cd.second;
+        bsoncxx::document::value doc = close_doc
+                                       << bsoncxx::builder::stream::finalize;
+
+        collection.insert_one(doc.view());
+
+        return true;
+    } catch (...) {
+        return false;
     }
-    auto close_doc = init_doc << bsoncxx::builder::stream::close_document;
-
-    bsoncxx::document::value doc = close_doc
-                                   << bsoncxx::builder::stream::finalize;
-
-    collection.insert_one(doc.view());
-
-    return true;
 }
