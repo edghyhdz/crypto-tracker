6750a2e20924adc03489a7fcaf6961e4a844054f
edghyhdz <edgar.hdz@hotmail.com>
2021-12-21 00:56:33 +0100

    Added first draft of get_token_price_data method definition and ApiManager constructor

---
+++ src/manager.cpp
@@ -0,0 +1,32 @@
+#include "manager.h"
+#include <iostream>
+#include <thread>
+
+/*
+ * Class constructor
+ */
+ApiManager::ApiManager(std::string uri, std::string db_name) {
+    this->_db_handler = new MongoDB(uri, db_name);
+    this->_binance_handler = new BinanceAPI(1);
+    std::cout << "Api manager constructor\n";
+}
+
+/**
+ * Requests token price data from given Exchange (Binance, Kucoin, etc ...)
+ *
+ * @param[in] api_type
+ *  wnge to get currency info from
+ * @return void
+ */
+void ApiManager::get_token_price_data(Exchange ex) {
+    std::cout << "Started getting Exchange data" << std::endl;
+    while (true) {
+        if (ex == Exchange::BINANCE) {
+            std::cout << "Save binance data" << std::endl;
+        } else {
+            std::cout << "Saving kucoin data\n";
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
