390518321973b78ac2e2812e5f32c46bdbea2a81
edghyhdz <edgar.hdz@hotmail.com>
2021-12-21 02:09:27 +0100

    Defined _get_request() method

--- include/binance_api.h
+++ include/binance_api.h
@@ -4,14 +4,20 @@
 #ifndef BINANCE_API
 #define BINANCE_API
 
+#include <iostream>
 #include "base_api.h"
+
+constexpr char BINANCE_ENDPOINT[] = "https://api.binance.com/api/v3/ticker/price";
+constexpr int REQUEST_RATE = 500;
 
 class BinanceAPI : public BaseAPIExchange {
    public:
     BinanceAPI(int id);
+    ~BinanceAPI() override { std::cout << "Called BinanceAPI destructor\n"; };
     std::string get_data() override;
     std::string process_response(std::string &response) override;
-    
+
    private:
+    void _get_request(long *http_code, std::string *read_buffer);
 };
 #endif
