3710b4cf0f14f7f3c118192d63f52ec31adb239e
edghyhdz <edgar.hdz@hotmail.com>
2021-12-21 02:08:50 +0100

    Added Destructor and get request to get_token_price_data method

--- src/manager.cpp
+++ src/manager.cpp
@@ -2,20 +2,29 @@
 #include <iostream>
 #include <thread>
 
-/*
+/**
  * Class constructor
  */
 DataManager::DataManager(std::string uri, std::string db_name) {
     this->_db_handler = new MongoDB(uri, db_name);
     this->_binance_handler = new BinanceAPI(1);
-    std::cout << "Api manager constructor\n";
+    std::cout << "DataManager constructor\n";
+}
+
+/**
+ * DataManager class destructor
+ */
+DataManager::~DataManager() {
+    delete this->_db_handler;
+    delete this->_binance_handler;
+    std::cout << "DataManager Destructor\n";
 }
 
 /**
  * Requests token price data from given Exchange (Binance, Kucoin, etc ...)
  *
- * @param[in] api_type
- *  wnge to get currency info from
+ * @param[in] ex
+ *  Exchange to get currency info from (i.e., Kucoin, Binance, etc)
  * @return void
  */
 void DataManager::get_token_price_data(Exchange ex) {
@@ -23,10 +32,13 @@ void DataManager::get_token_price_data(Exchange ex) {
     while (true) {
         if (ex == Exchange::BINANCE) {
             std::cout << "Save binance data" << std::endl;
+            auto read_buffer = this->_binance_handler->get_data();
+            std::cout << "Read buffer:\n" << read_buffer << std::endl;
+
         } else {
             std::cout << "Saving kucoin data\n";
         }
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
 
