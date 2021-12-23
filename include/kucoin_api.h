0c69ad24c0d59dc67e9085a5e7136c5dfe26bb70
edghyhdz <edgar.hdz@hotmail.com>
2021-12-23 16:34:41 +0100

    Added defautl codec structs for KuCoin api response to be parsed by spotify::json

--- include/kucoin_api.h
+++ include/kucoin_api.h
@@ -21,8 +21,32 @@ constexpr char KUCOIN_TICKER_ENDPOINT[] = "https://api.kucoin.com/api/v1/market/
 constexpr int REQUEST_LIMIT_CODE = 403;  // Error code to expect if request limit is surpassed
 constexpr int REQUEST_RATE = 1000;  // Request new data (in milliseconds)
 
+struct Ticker {
+    std::string symbol;
+    std::string buy; 
+    std::string sell;
+    std::string change_rate;
+    std::string change_price;
+    std::string high;
+    std::string low;
+    std::string vol;
+    std::string vol_value;
+    std::string last;
+    std::string average_price;
+    std::string taker_fee_rate;
+    std::string maker_fee_rate;
+    std::string taker_coefficient;
+    std::string maker_coefficient;
+};
+
+struct ResponseData {
+    long time;
+    std::vector<yact::KuCoin::Ticker> ticker;
+};
+
 struct Response {
-    std::vector<std::map<std::string, std::string>> response;
+    std::string code;
+    ResponseData data;
 };
 
 }  // namespace KuCoin
@@ -55,10 +79,45 @@ template <>
 struct default_codec_t<yact::KuCoin::Response> {
     static codec::object_t<yact::KuCoin::Response> codec() {
         auto codec = codec::object<yact::KuCoin::Response>();
-        codec.required("response", &yact::KuCoin::Response::response);
+        codec.required("code", &yact::KuCoin::Response::code);
+        codec.required("data", &yact::KuCoin::Response::data);
         return codec;
     }
 };
+
+template <>
+struct default_codec_t<yact::KuCoin::ResponseData> {
+    static codec::object_t<yact::KuCoin::ResponseData> codec() {
+        auto codec = codec::object<yact::KuCoin::ResponseData>();
+        codec.required("time", &yact::KuCoin::ResponseData::time);
+        codec.required("ticker", &yact::KuCoin::ResponseData::ticker);
+        return codec;
+    }
+};
+
+template <>
+struct default_codec_t<yact::KuCoin::Ticker> {
+    static codec::object_t<yact::KuCoin::Ticker> codec() {
+        auto codec = codec::object<yact::KuCoin::Ticker>();
+        codec.required("symbolName", &yact::KuCoin::Ticker::symbol);
+        codec.required("buy", &yact::KuCoin::Ticker::buy);
+        codec.required("sell", &yact::KuCoin::Ticker::sell);
+        codec.required("changeRate", &yact::KuCoin::Ticker::change_rate);
+        codec.required("changePrice", &yact::KuCoin::Ticker::change_price);
+        codec.required("high", &yact::KuCoin::Ticker::high);
+        codec.required("low", &yact::KuCoin::Ticker::low);
+        codec.required("vol", &yact::KuCoin::Ticker::vol);
+        codec.required("volValue", &yact::KuCoin::Ticker::vol_value);
+        codec.required("last", &yact::KuCoin::Ticker::last);
+        codec.required("averagePrice", &yact::KuCoin::Ticker::average_price);
+        codec.required("takerFeeRate", &yact::KuCoin::Ticker::taker_fee_rate);
+        codec.required("makerFeeRate", &yact::KuCoin::Ticker::maker_fee_rate);
+        codec.required("takerCoefficient", &yact::KuCoin::Ticker::taker_coefficient);
+        codec.required("makerCoefficient", &yact::KuCoin::Ticker::maker_coefficient);
+        return codec;
+    }
+};
+
 }  // namespace json
 }  // namespace spotify
 #endif
