37a4ee6b7e33465c4d3de430aa49f632ce73d7cc
edghyhdz <edgar.hdz@hotmail.com>
2021-12-23 16:35:13 +0100

    First main draft

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+#include "binance_api.h"
+#include "manager.h"
+#include "mongo_db.h"
+
+int main(int argc, char** argv) {
+    std::string uri{"mongodb://0.0.0.0:27017"};
+    std::string db_name{"testDB"};
+
+    yact::DataManager data_handler = yact::DataManager(uri, db_name);
+
+    std::thread t = std::thread(&yact::DataManager::get_token_price_data,
+                                &data_handler, yact::Exchange::BINANCE);
+
+    std::thread t_k = std::thread(&yact::DataManager::get_token_price_data,
+                                  &data_handler, yact::Exchange::KUCOIN);
+
+    t.join();
+    t_k.join();
+}
