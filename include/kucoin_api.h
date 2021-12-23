8dac21a2a667ef4802c85b391ecc5e407b2e7e4e
edghyhdz <edgar.hdz@hotmail.com>
2021-12-23 04:28:40 +0100

    Added kucoin namespace

--- include/kucoin_api.h
+++ include/kucoin_api.h
@@ -1,5 +1,5 @@
 /**
- * Binance API class handler
+ * KuCoin API class declaration
  */
 #ifndef KUCOIN_API
 #define KUCOIN_API
@@ -13,36 +13,23 @@
 #include <spotify/json/encode.hpp>
 #include "base_api.h"
 
-struct Response {
-    std::vector<std::map<std::string, std::string>> response;
-};
-
-namespace spotify {
-namespace json {
-
-// Specialize spotify::json::default_codec_t to specify default behavior when
-// encoding and decoding objects of certain types.
-template <>
-struct default_codec_t<Response> {
-    static codec::object_t<Response> codec() {
-        auto codec = codec::object<Response>();
-        codec.required("response", &Response::response);
-        return codec;
-    }
-};
-}  // namespace json
-}  // namespace spotify
-
 namespace yact {
+
+namespace KuCoin {
 
 constexpr char KUCOIN_TICKER_ENDPOINT[] = "https://api.kucoin.com/api/v1/market/allTickers";
 constexpr int REQUEST_LIMIT_CODE = 403;  // Error code to expect if request limit is surpassed
 constexpr int REQUEST_RATE = 1000;  // Request new data (in milliseconds)
 
+struct Response {
+    std::vector<std::map<std::string, std::string>> response;
+};
+
+}  // namespace KuCoin
 class KucoinAPI : BaseAPIExchange {
    public:
     KucoinAPI(int id);
-    ~KucoinAPI() override { std::cout << "Called KucoinAPI destructor\n"; }
+    ~KucoinAPI() override;
     std::map<std::string, double> get_data() override;
     std::map<std::string, double> process_token_price_response(
         std::string &response) override;
@@ -51,14 +38,28 @@ class KucoinAPI : BaseAPIExchange {
     void set_current_request_code(int r_code);
     int get_current_request_code();
 
+    void _get_request(long *http_code, std::string *read_buffer, std::string end_point) override;
+   
    private:
-    void _get_request(long *http_code, std::string *read_buffer);
     int _current_request_code;
     std::mutex *_mtx;
-
 };
-
 }  // namespace yact
 
+namespace spotify {
+namespace json {
+
+// Specialize spotify::json::default_codec_t to specify default behavior when
+// encoding and decoding objects of certain types.
+template <>
+struct default_codec_t<yact::KuCoin::Response> {
+    static codec::object_t<yact::KuCoin::Response> codec() {
+        auto codec = codec::object<yact::KuCoin::Response>();
+        codec.required("response", &yact::KuCoin::Response::response);
+        return codec;
+    }
+};
+}  // namespace json
+}  // namespace spotify
 #endif
 
