053a9a29b7407e7215c925a1d6b4ab3b47b2cb22
edghyhdz <edgar.hdz@hotmail.com>
2021-12-23 04:29:57 +0100

    Added inline call back WriteCallBack() for get requests

--- include/base_api.h
+++ include/base_api.h
@@ -8,7 +8,14 @@
 #include <map>
 
 // Forward struct declaration
-struct Response;
+// struct Response;
+
+// Callback to curl function
+// Reference https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+inline size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string *)userp)->append((char *)contents, size * nmemb);
+    return size * nmemb;
+}
 
 class BaseAPIExchange {
    public:
@@ -17,15 +24,16 @@ class BaseAPIExchange {
     // Do get request and get Token/coin price data
     virtual std::map<std::string, double> get_data() = 0;
     // Process get response
-    virtual std::map<std::string, double> process_token_price_response(Response &response) = 0;
+    virtual std::map<std::string, double> process_token_price_response(std::string &response) = 0;
 
     // Normal behavior methods
     virtual int get_id() { return _id; }
     virtual std::string get_api_key() { return _api_key; }
 
     virtual void set_id(int id) { this->_id = id; }
-
+    // virtual void _get_request(long *http_code, std::string *read_buffer, std::string end_point);
    protected:
+    virtual void _get_request(long *http_code, std::string *read_buffer, std::string end_point) = 0;
     int _id;
 
    private:
