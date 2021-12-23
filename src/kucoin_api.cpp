aad6ef88f54334f634ec4003a6db2e5d2db616fa
edghyhdz <edgar.hdz@hotmail.com>
2021-12-23 16:33:10 +0100

    Added set_current_request_code() and get_current_request_code() method definitions. Added get_data() method full definition

--- src/kucoin_api.cpp
+++ src/kucoin_api.cpp
@@ -5,13 +5,9 @@
 #include <boost/algorithm/string.hpp>
 #include <sstream>
 
-// Callback to curl function
-// Reference https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
-// size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp) {
-//    ((std::string *)userp)->append((char *)contents, size * nmemb);
-//    return size * nmemb;
-// }
-
+/**
+ * Class constructor
+ */
 yact::KucoinAPI::KucoinAPI(int id) {
     this->set_id(id);
     this->_mtx = new std::mutex();
@@ -20,45 +16,56 @@ yact::KucoinAPI::KucoinAPI(int id) {
 /*
  * Class destructor
  */
-yact::KucoinAPI::~KucoinAPI(){
-    delete this->_mtx;
-}
-
+yact::KucoinAPI::~KucoinAPI() { delete this->_mtx; }
 
 /**
  * Process get request response into json format
  *
- * @params[in] response
+ * @params[in] read_buffer
  *  Get request response
  */
 std::map<std::string, double> yact::KucoinAPI::process_token_price_response(
     std::string &read_buffer) {
     // Change keys to symbol name
-
-    yact::KuCoin::Response res = spotify::json::decode<yact::KuCoin::Response>(read_buffer);
+    yact::KuCoin::Response res =
+        spotify::json::decode<yact::KuCoin::Response>(read_buffer);
 
     std::map<std::string, double> processed_response;
-    for (auto item : res.response) {
+
+    for (auto item : res.data.ticker) {
         processed_response.insert(
-            std::make_pair(item.at("symbol"), std::stod(item.at("price"))));
+            std::make_pair(item.symbol, std::stod(item.last)));
     }
 
     return processed_response;
 }
 
-/**/
-std::map<std::string, double> yact::KucoinAPI::get_data(){
+/**
+ * Get symbol price data of all tokens on Exchange
+ */
+std::map<std::string, double> yact::KucoinAPI::get_data() {
     long http_code;
     std::string read_buffer;
-    
-    std::map<std::string, double> delete_map; 
-    // TODO: delete ^
 
     // Request token price data
-    this->_get_request(&http_code, &read_buffer, yact::KuCoin::KUCOIN_TICKER_ENDPOINT);
-    std::cout << "KuCoin response:\n" << read_buffer << std::endl;
+    this->_get_request(&http_code, &read_buffer,
+                       yact::KuCoin::KUCOIN_TICKER_ENDPOINT);
     
-    return delete_map;
+    this->set_current_request_code(http_code);
+    std::cout << "Current request code Kucoin: "
+              << this->get_current_request_code() << std::endl;
+    
+    return this->process_token_price_response(read_buffer);
+}
+
+void yact::KucoinAPI::set_current_request_code(int r_code) {
+    std::lock_guard<std::mutex>(*this->_mtx);
+    this->_current_request_code = r_code;
+}
+
+int yact::KucoinAPI::get_current_request_code() {
+    std::lock_guard<std::mutex>(*this->_mtx);
+    return this->_current_request_code;
 }
 
 /**
@@ -70,17 +77,15 @@ std::map<std::string, double> yact::KucoinAPI::get_data(){
  *  Response text
  * @return none
  */
-void yact::KucoinAPI::_get_request(long *http_code, std::string *read_buffer, std::string end_point){
+void yact::KucoinAPI::_get_request(long *http_code, std::string *read_buffer,
+                                   std::string end_point) {
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_URL, end_point.c_str());
-    // Used to write response header data back - debug purposes
-    curl_easy_setopt(curl, CURLOPT_HEADER, 1);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, read_buffer);
     curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, http_code);
     curl_easy_cleanup(curl);
 }
-
 
