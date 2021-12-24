98233b7b78a9224c0ba9e7ec596cb885c7866b4b
edghyhdz <edgar.hdz@hotmail.com>
2021-12-24 02:53:35 +0100

    Changed DataManager main method

--- main.cpp
+++ main.cpp
@@ -10,12 +10,14 @@ int main(int argc, char** argv) {
 
     yact::DataManager data_handler = yact::DataManager(uri, db_name);
 
-    std::thread t = std::thread(&yact::DataManager::get_token_price_data,
-                                &data_handler, yact::Exchange::BINANCE);
+    std::thread t = std::thread(&yact::DataManager::run_all_exchange_apis, &data_handler);
 
-    std::thread t_k = std::thread(&yact::DataManager::get_token_price_data,
-                                  &data_handler, yact::Exchange::KUCOIN);
+    // std::thread t = std::thread(&yact::DataManager::get_token_price_data,
+    //                            &data_handler, yact::Exchange::BINANCE);
+
+    // std::thread t_k = std::thread(&yact::DataManager::get_token_price_data,
+    //                              &data_handler, yact::Exchange::KUCOIN);
 
     t.join();
-    t_k.join();
+    //t_k.join();
 }
