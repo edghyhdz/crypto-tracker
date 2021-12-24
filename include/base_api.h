7f33d09597f22fb4fae9de34214ce5d1b458d8a0
edghyhdz <edgar.hdz@hotmail.com>
2021-12-24 23:26:34 +0100

    Added setters and getters for http code - used to check API limits

--- include/base_api.h
+++ include/base_api.h
@@ -20,21 +20,39 @@ class BaseAPIExchange {
 
     // Do get request and get Token/coin price data
     virtual std::map<std::string, double> get_data() = 0;
+   
     // Process get response
     virtual std::map<std::string, double> process_token_price_response(std::string &response) = 0;
+    
+    // Handle by derived class - check whether requests can proceed or not
+    virtual bool has_reached_request_limit() = 0;
 
     // Normal behavior methods
     virtual int get_id() { return _id; }
     virtual std::string get_api_key() { return _api_key; }
 
+    virtual std::string get_api_name() { return _api_name; }
     virtual void set_id(int id) { this->_id = id; }
-    // virtual void _get_request(long *http_code, std::string *read_buffer, std::string end_point);
+
+    // Setter & getter http code - used to check for Exchange API limits
+    virtual void set_http_code(int http_code) { this->_http_code = http_code; }
+    virtual int get_http_code() { return this->_http_code; }
+    
+    // http error counter - check if limit has been exceeded several times
+    // consecutively
+    virtual void increase_http_limit_counter() { this->_http_limit_counter++; }
+    virtual void reset_http_limit_counter() { this->_http_limit_counter = 0; }
+    virtual int get_http_limit_counter() { return this->_http_limit_counter; }
+   
    protected:
     virtual void _get_request(long *http_code, std::string *read_buffer, std::string end_point) = 0;
+    std::string _api_name;
     int _id;
 
    private:
     std::map<std::string, double> _processed_response;
+    int _http_code{200};
+    int _http_limit_counter{0};
     std::string _api_key;
 };
 
