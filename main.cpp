b3cad8b95e976c4b767025cb4716fca1f282ca56
edghyhdz <edgar.hdz@hotmail.com>
2021-12-27 20:14:19 +0100

    Deleted unused headers. Added CheckOrders class instance

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <thread>
-#include "binance_api.h"
 #include "manager.h"
-#include "mongo_db.h"
+#include "orders.h"
 
 int main(int argc, char** argv) {
     std::string uri{"mongodb://0.0.0.0:27017"};
-    std::string db_name{"testDB"};
+    std::string db_name{"cryptoDB"};
 
     yact::DataManager data_handler = yact::DataManager(uri, db_name);
+    yact::Exchange bi = yact::Exchange::BINANCE;
+    yact::CheckOrders order = yact::CheckOrders(bi, uri, db_name);
 
     std::thread t = std::thread(&yact::DataManager::run_all_exchange_apis, &data_handler);
 
