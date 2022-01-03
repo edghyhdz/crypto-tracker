4cd225c4c3a95acabdbe14594ce4166bdbd73e9e
edghyhdz <edgar.hdz@hotmail.com>
2022-01-03 08:27:33 +0100

    Added get_record() method

--- include/base_db.h
+++ include/base_db.h
@@ -17,5 +17,6 @@ class BaseDB {
     virtual bool add_record(std::string table, Data data) = 0;
     virtual bool update_record() = 0;
     virtual bool delete_record(std::string table, Data data) = 0;
+    virtual bool get_record(std::string table, Data data) = 0;
 };
 #endif
