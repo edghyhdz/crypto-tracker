4fff3d0b3924fea8ef548791f8083c0da953037a
edghyhdz <edgar.hdz@hotmail.com>
2022-01-04 16:27:32 +0100

    open_order method definition and added json struct decoders for database query output

--- include/orders.h
+++ include/orders.h
@@ -22,19 +22,83 @@
 
 namespace yact {
 
+struct OrderData {
+   double price;
+   double qty;
+   bool executed;
+};
+
+struct UserOrders {
+   std::string _uid;
+   std::string exchange;
+   bool has_orders;
+   std::map<std::string, std::vector<OrderData>> sell_orders;
+   std::map<std::string, std::vector<OrderData>> buy_orders;
+};
+
+ struct AllUsers {
+    std::vector<UserOrders> all_orders;
+ };
+
 enum class Exchange;  // Forward declaration
+
+enum class OrderType { BUY, SELL, BUY_OR_SELL };
 
 class CheckOrders {
    public:
-    CheckOrders(Exchange ex, std::string uri, std::string db_name);
+    CheckOrders(std::string exchange_name, std::string uri, std::string db_name);
     ~CheckOrders();
+    void open_orders(OrderType o_type, std::string *buffer_data);
+
+    std::map<std::string, double> get_symbol_prices();
     void get_exchange_crypto_symbols();  // Get all symbol names from exchange
     std::vector<std::string> get_user_cryptos();
 
    private:
+    std::string _exchange_name;
     std::shared_ptr<yact::MongoDB> _db_h;
-    mongocxx::instance _instance;
 };
 }  // namespace yact
+
+namespace spotify {
+namespace json {
+
+// Specialize spotify::json::default_codec_t to specify default behavior when
+// encoding and decoding objects of certain types.
+template <> struct default_codec_t<yact::AllUsers> {
+  static codec::object_t<yact::AllUsers> codec() {
+    auto codec = codec::object<yact::AllUsers>();
+    codec.required("results", &yact::AllUsers::all_orders);
+    return codec;
+  }
+};
+
+// Specialize spotify::json::default_codec_t to specify default behavior when
+// encoding and decoding objects of certain types.
+template <> struct default_codec_t<yact::UserOrders> {
+  static codec::object_t<yact::UserOrders> codec() {
+    auto codec = codec::object<yact::UserOrders>();
+    codec.required("uid", &yact::UserOrders::_uid);
+    codec.required("exchange", &yact::UserOrders::exchange);
+    codec.required("hasOrders", &yact::UserOrders::has_orders);
+    codec.optional("sellOrders", &yact::UserOrders::sell_orders);
+    codec.optional("buyOrders", &yact::UserOrders::buy_orders);
+    return codec;
+  }
+};
+// Specialize spotify::json::default_codec_t to specify default behavior when
+// encoding and decoding objects of certain types.
+template <> struct default_codec_t<yact::OrderData> {
+  static codec::object_t<yact::OrderData> codec() {
+    auto codec = codec::object<yact::OrderData>();
+    codec.required("price", &yact::OrderData::price);
+    codec.required("qty", &yact::OrderData::qty);
+    codec.required("executed", &yact::OrderData::executed);
+    return codec;
+  }
+};
+} // namespace json
+} // namespace spotify
+
 #endif
 
--- src/orders.cpp
+++ src/orders.cpp
@@ -1,13 +1,45 @@
 #include "orders.h"
 #include "log.h"
 
-yact::CheckOrders::CheckOrders(Exchange ex, std::string uri, std::string db){
-   this->_db_h = std::make_shared<MongoDB>(uri, db);
-   LOG(level::Info) << "CheckOrders constructor" << std::endl;
+yact::CheckOrders::CheckOrders(std::string exchange_name, std::string uri,
+                               std::string db) {
+    this->_exchange_name = exchange_name;
+    this->_db_h = std::make_shared<MongoDB>(uri, db);
+
+    LOG(level::Info) << "CheckOrders constructor" << std::endl;
 }
 
 yact::CheckOrders::~CheckOrders() {}
 
-void yact::CheckOrders::get_exchange_crypto_symbols() {
-    return;
+/**
+ * Search for BUY, SELL or BUY & SELL open orders.
+ * @param[in] o_type
+ *  Order Type to search on database
+ * @param[in, out] buffer_data
+ *  returned data from db to be parsed as json
+ */
+void yact::CheckOrders::open_orders(OrderType o_type,
+                                    std::string *buffer_data) {
+    Data data;
+    data.filters = "";
+
+    std::string buffer;
+    bsoncxx::builder::stream::document doc{};
+
+    auto doc_view = doc << "exchange" << this->_exchange_name 
+                        << bsoncxx::builder::stream::finalize;
+
+    // Pass it as a document view otherwise object will get destroyed after
+    // leaving this scope
+    data.document_view = doc_view.view();
+    this->_db_h->get_record("userOrders", data, buffer_data);
+
+    yact::AllUsers all = spotify::json::decode<yact::AllUsers>(*buffer_data);
+
+    for (yact::UserOrders item : all.all_orders) {
+        std::cout << "Size buy orders: " << item.buy_orders.size() << std::endl;
+        std::cout << "Size sell orders: " << item.sell_orders.size() << std::endl;
+    }
 }
+
+void yact::CheckOrders::get_exchange_crypto_symbols() { return; }
