a8a52cfda6781f31289752f02d6337883d8444c4
edghyhdz <edgar.hdz@hotmail.com>
2022-01-22 14:35:14 +0100

    Added CheckOrder instance - check users' open buy/sell orders

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ int main(int argc, char** argv) {
     std::string db_name{"cryptoDB"};
 
     yact::DataManager data_handler = yact::DataManager(uri, db_name);
-    yact::Exchange bi = yact::Exchange::BINANCE;
-    yact::CheckOrders order = yact::CheckOrders(bi, uri, db_name);
+    // yact::Exchange bi = yact::Exchange::BINANCE;
+    yact::CheckOrders order = yact::CheckOrders("kucoin", uri, db_name);
+    std::string buf_data;
+    order.open_orders(yact::OrderType::BUY, &buf_data);
 
-    std::thread t = std::thread(&yact::DataManager::run_all_exchange_apis, &data_handler);
+    std::thread t =
+        std::thread(&yact::DataManager::run_all_exchange_apis, &data_handler);
 
     t.join();
 }
